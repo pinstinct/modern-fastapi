20ddb14d5ab5eb292f21301b0241fae3f8d82762
pinstinct <contact@limhm.dev>
2025-05-30 17:52:50 +0900

    다중 라우터

--- src/main.py
+++ src/main.py
@@ -1,17 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
+from web import explorer
 
 app = FastAPI()
-
-
-@app.get("/")
-def top():
-    return "top here"
-
-
-@app.get("/echo/{thing}")
-def echo(thing):
-    return f"echoing {thing}"
+app.include_router(explorer.router)
 
 
 if __name__ == "__main__":
