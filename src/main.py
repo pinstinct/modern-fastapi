3469a5d6622c44708865034847b7239764852946
pinstinct <contact@limhm.dev>
2025-06-02 10:30:38 +0900

    endpoint 추가

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
-from web import explorer
+from web import explorer, creature
 
 app = FastAPI()
 app.include_router(explorer.router)
+app.include_router(creature.router)
 
 
 if __name__ == "__main__":
--- src/web/explorer.py
+++ src/web/explorer.py
@@ -1,8 +1,37 @@
 from fastapi import APIRouter
 
+from model.explorer import Explorer
+from fake import explorer as service
+
 router = APIRouter(prefix="/explorer")
 
 
 @router.get("/")
-def top():
-    return "top explorer endpoint"
+def get_all() -> list[Explorer]:
+    return service.get_all()
+
+
+@router.get("/{name}")
+def get_one(name) -> Explorer | None:
+    return service.get_one(name)
+
+
+# 나머지 엔드포인트. 현재 아무 일도 하지 않는다.
+@router.post("/")
+def create(explorer: Explorer) -> Explorer:
+    return service.create(explorer)
+
+
+@router.patch("/{name}")
+def modify(name, explorer: Explorer) -> Explorer:
+    return service.modify(name, explorer)
+
+
+@router.patch("/{name}")
+def replace(name, explorer: Explorer) -> Explorer:
+    return service.replace(name, explorer)
+
+
+@router.delete("/{name}")
+def delete(name: str):
+    return None
