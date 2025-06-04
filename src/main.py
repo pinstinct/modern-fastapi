e62d1cabc880aa178936b8a6e1dea9cae7b190b9
pinstinct <contact@limhm.dev>
2025-06-04 10:58:20 +0900

    라우터 등록

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
-from web import explorer, creature
+from web import explorer, creature, user
 
 app = FastAPI()
 app.include_router(explorer.router)
 app.include_router(creature.router)
+app.include_router(user.router)
 
 
 if __name__ == "__main__":
